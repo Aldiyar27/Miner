2f5be4e0bc9af091b4f25c201823d2c085178a18
АшкеевАл <@004-08>
2023-04-10 16:29:58 +0600

    Add project files.

---
+++ Miner/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Miner
+{
+    partial class GameMiner
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.главнаяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новаяИгрыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.загрузитьИгруToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьИгруToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.помошьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.главнаяToolStripMenuItem,
+            this.играToolStripMenuItem,
+            this.помошьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(741, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // главнаяToolStripMenuItem
+            // 
+            this.главнаяToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИгрыToolStripMenuItem,
+            this.загрузитьToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.загрузитьИгруToolStripMenuItem,
+            this.сохранитьИгруToolStripMenuItem});
+            this.главнаяToolStripMenuItem.Name = "главнаяToolStripMenuItem";
+            this.главнаяToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.главнаяToolStripMenuItem.Text = "Главная ";
+            // 
+            // новаяИгрыToolStripMenuItem
+            // 
+            this.новаяИгрыToolStripMenuItem.Name = "новаяИгрыToolStripMenuItem";
+            this.новаяИгрыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.новаяИгрыToolStripMenuItem.Text = "Новая игры";
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьToolStripMenuItem.Text = "Перезапуск";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // загрузитьИгруToolStripMenuItem
+            // 
+            this.загрузитьИгруToolStripMenuItem.Name = "загрузитьИгруToolStripMenuItem";
+            this.загрузитьИгруToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьИгруToolStripMenuItem.Text = "Загрузить игру";
+            // 
+            // сохранитьИгруToolStripMenuItem
+            // 
+            this.сохранитьИгруToolStripMenuItem.Name = "сохранитьИгруToolStripMenuItem";
+            this.сохранитьИгруToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьИгруToolStripMenuItem.Text = "Сохранить игру";
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // помошьToolStripMenuItem
+            // 
+            this.помошьToolStripMenuItem.Name = "помошьToolStripMenuItem";
+            this.помошьToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.помошьToolStripMenuItem.Text = "Помошь";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(22, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(489, 472);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(589, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(648, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(22, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(576, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Размер роля:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(617, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "7";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(595, 499);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "☽ / ☼";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(576, 480);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Сменить тему:";
+            // 
+            // GameMiner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(741, 593);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GameMiner";
+            this.Text = "Miner";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem главнаяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новаяИгрыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьИгруToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьИгруToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem помошьToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Miner/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Miner
+{
+    public partial class GameMiner : Form
+    {
+        public static int mapSize = 7;
+        public const int cellSize = 70;
+        private static int currentPictureToSet = 0;
+        public static int[,] map = new int[mapSize, mapSize];
+        public static Button[,] buttons = new Button[mapSize, mapSize];
+        private static bool isFirstStep;
+        private static Point firstCoord;
+        public static Form form;
+        public static Panel panel;
+        public static bool DarkMode = false;
+
+        public GameMiner()
+        {
+            InitializeComponent();
+
+            Game(panel1);
+        }
+
+        private static void ConfigureMapSize(Panel current)
+        {
+            current.Width = mapSize * cellSize;
+            current.Height = mapSize * cellSize;
+        }
+
+        private static void Map()
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    map[i, j] = 0;
+                }
+            }
+        }
+
+        public static void Game(Panel current)
+        {
+            panel = current;
+            currentPictureToSet = 0;
+            isFirstStep = true;
+            ConfigureMapSize(current);
+            Map();
+            Buttons(current);
+        }
+
+        private static void Buttons(Panel current)
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    Button button = new Button();
+                    button.Location = new Point(j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    button.Image = GetImg(0);
+                    button.MouseUp += new MouseEventHandler(OnButtonPressedMouse);
+                    current.Controls.Add(button);
+                    buttons[i, j] = button;
+                }
+            }
+        }
+
+        private static void OnButtonPressedMouse(object sender, MouseEventArgs e)
+        {
+            Button pressedButton = sender as Button;
+            switch (e.Button.ToString())
+            {
+                case "Right":
+                    OnRightButtonPressed(pressedButton);
+                    break;
+                case "Left":
+                    OnLeftButtonPressed(pressedButton);
+                    break;
+            }
+        }
+
+        private static void OnRightButtonPressed(Button pressedButton)
+        {
+            currentPictureToSet++;
+            currentPictureToSet %= 2;
+            int num = 0;
+
+            switch (currentPictureToSet)
+            {
+                case 0:
+                    num = 0;
+                    break;
+                case 1:
+                    num = -2;
+                    break;
+            }
+            pressedButton.Image = GetImg(num);
+        }
+
+        private static Image GetImg(int num)
+        {
+            return Image.FromFile($"C:/Users/АшкеевАл/Downloads/Miner-master/Miner/Sprites/{num}.png");
+
+        }
+
+        private static void OnLeftButtonPressed(Button pressedButton)
+        {
+            pressedButton.Enabled = false;
+            int iButton = pressedButton.Location.Y / cellSize;
+            int jButton = pressedButton.Location.X / cellSize;
+            if (isFirstStep)
+            {
+                firstCoord = new Point(jButton, iButton);
+                SpawnBomb();
+                CountBomb();
+                isFirstStep = false;
+            }
+            OpenCells(iButton, jButton);
+
+            if (map[iButton, jButton] == -1)
+            {
+                AllBombs(iButton, jButton);
+                MessageBox.Show("Поражение!");
+                panel.Controls.Clear();
+                Game(panel);
+            }
+        }
+
+        private static void AllBombs(int iBomb, int jBomb)
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (i == iBomb && j == jBomb)
+                        continue;
+                    if (map[i, j] == -1)
+                    {
+                        buttons[i, j].Image = GetImg(-1);
+                    }
+                }
+            }
+        }
+
+        private static void SpawnBomb()
+        {
+            Random r = new Random();
+            int number = mapSize + 1;
+
+            for (int i = 0; i < number; i++)
+            {
+                int posI = r.Next(0, mapSize - 1);
+                int posJ = r.Next(0, mapSize - 1);
+
+                while (map[posI, posJ] == -1 || (Math.Abs(posI - firstCoord.Y) <= 1 && Math.Abs(posJ - firstCoord.X) <= 1))
+                {
+                    posI = r.Next(0, mapSize - 1);
+                    posJ = r.Next(0, mapSize - 1);
+                }
+                map[posI, posJ] = -1;
+            }
+        }
+
+        private static void CountBomb()
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (map[i, j] == -1)
+                    {
+                        for (int k = i - 1; k < i + 2; k++)
+                        {
+                            for (int l = j - 1; l < j + 2; l++)
+                            {
+                                if (!IsInBorder(k, l) || map[k, l] == -1)
+                                    continue;
+                                map[k, l] = map[k, l] + 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void OpenCell(int i, int j)
+        {
+            buttons[i, j].Enabled = false;
+
+            switch (map[i, j])
+            {
+                case 1:
+                    buttons[i, j].Image = GetImg(1);
+                    break;                
+                case 2:
+                    buttons[i, j].Image = GetImg(2);
+                    break;                
+                case 3:                   
+                    buttons[i, j].Image = GetImg(3);
+                    break;                
+                case 4:
+                    buttons[i, j].Image = GetImg(4);
+                    break;                
+                case 5:                   
+                    buttons[i, j].Image = GetImg(5);
+                    break;                
+                case 6:                   
+                    buttons[i, j].Image = GetImg(6);
+                    break;                
+                case 7:                   
+                    buttons[i, j].Image = GetImg(7);
+                    break;                
+                case 8:                   
+                    buttons[i, j].Image = GetImg(8);
+                    break;
+                case -1:
+                    buttons[i, j].Image = GetImg(-1);
+                    break;
+                case 0:
+                    buttons[i, j].Image = GetImg(0);
+                    break;
+            }
+        }
+
+        private static void OpenCells(int i, int j)
+        {
+            OpenCell(i, j);
+
+            if (map[i, j] > 0)
+                return;
+
+            for (int k = i - 1; k < i + 2; k++)
+            {
+                for (int l = j - 1; l < j + 2; l++)
+                {
+                    if (!IsInBorder(k, l))
+                        continue;
+                    if (!buttons[k, l].Enabled)
+                        continue;
+                    if (map[k, l] == 0)
+                        OpenCells(k, l);
+                    else if (map[k, l] > 0)
+                        OpenCell(k, l);
+                }
+            }
+        }
+
+        private static bool IsInBorder(int i, int j)
+        {
+            if (i < 0 || j < 0 || j > mapSize - 1 || i > mapSize - 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(label2.Text);
+            if (num >= 6)
+            {
+                mapSize--;
+                label2.Text = mapSize.ToString();
+                panel.Controls.Clear();
+                Game(panel1);
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(label2.Text);
+            if (num <= 6)
+            {
+                mapSize++;
+                label2.Text = mapSize.ToString();
+                panel.Controls.Clear();
+                Game(panel1);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(DarkMode == false)
+            {
+                this.BackColor = Color.DimGray;
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+                label3.ForeColor = Color.White;
+                DarkMode = true;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+                label1.ForeColor = Color.Black;
+                label2.ForeColor = Color.Black;
+                label3.ForeColor = Color.Black;
+                DarkMode = false;
+            }
+        }
+    }
+}
